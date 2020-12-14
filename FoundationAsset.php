c40b83dd30416ec02b99fcda618a2af0b3191010
Gert van Tonder <gvanto@gmail.com>
2020-12-14 09:46:03 +0200

    Move js file to js assets - fix chrome error

--- FoundationAsset.php
+++ FoundationAsset.php
@@ -25,19 +25,18 @@ class FoundationAsset extends AssetBundle
         //'https://cdn.jsdelivr.net/foundation/6.1.1/foundation.min.css', 
         // For CSS autocompleting, we'll use direct files instead of CDN
         'css/foundation.min.css',
-        // Motion UI: http://foundation.zurb.com/sites/docs/motion-ui.html
-        'https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.2.3/motion-ui.min.js'
-        
     ];
-    // JS awesomeness: http://www.jsdelivr.com/projects/foundation
+    
     public $js = [
         //'https://cdn.jsdelivr.net/foundation/6.1.1/foundation.min.js',   // The core Foundation JS
         // We will use Yii2's built-in front and backend validation instead of abide
         //'https://cdn.jsdelivr.net/foundation/6.1.1/js/foundation.abide.js',
         'js/foundation.min.js',
-        'js/foundationize.js',        
+        'js/foundationize.js',
+        // Motion UI: http://foundation.zurb.com/sites/docs/motion-ui.html
+        'https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.2.3/motion-ui.min.js'
     ];
-    // Depends on jQuery
+    
     public $depends = [
         'yii\web\JqueryAsset'
     ]; 
